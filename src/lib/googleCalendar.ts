9972a453cf0f4832176a28560cab52d0799d45ac
HosokawaR <thin.river.jp@gmail.com>
2024-11-13 13:33:58 +0900

    wip

--- src/lib/googleCalendar.ts
+++ src/lib/googleCalendar.ts
@@ -8,6 +8,8 @@ import { google, calendar_v3 } from "googleapis";
 
 const FETCH_EVENTS_DURATION = 7 * 24 * 60 * 60 * 1000;
 
+const SUPER_KOYOMI_CALENDAR_NAME = "スーパーこよみで追加された予定";
+
 export class GoogleCalendar implements Calendar {
   private client: calendar_v3.Calendar;
 
@@ -30,7 +32,7 @@ export class GoogleCalendar implements Calendar {
       calendarId: primaryCalendarId!,
       timeMin: new Date().toISOString(),
       timeMax: new Date(Date.now() + FETCH_EVENTS_DURATION).toISOString(),
-      singleEvents: true,      
+      singleEvents: true,
       orderBy: "startTime",
     });
 
@@ -49,8 +51,61 @@ export class GoogleCalendar implements Calendar {
             ? event.status
             : null
           : null,
+        attendees:
+          event.attendees?.map((it) => ({ email: it.email ?? null })) ?? [],
       })) ?? []
     );
+  }
+
+  async addEvent(event: CalEvent): Promise<CalEvent> {
+    const { data: calendars } = await this.client.calendarList.list();
+    let superKoyomiCalendarId = calendars.items?.find(
+      (calendar) => calendar.summary === SUPER_KOYOMI_CALENDAR_NAME
+    )?.id;
+
+    if (!superKoyomiCalendarId) {
+      const result = await this.client.calendars.insert({
+        requestBody: { summary: SUPER_KOYOMI_CALENDAR_NAME },
+      });
+      superKoyomiCalendarId = result.data.id;
+    }
+
+    if (!superKoyomiCalendarId) {
+      throw new Error("Super Koyomi calendar not found");
+    }
+
+    console.log(event.start.toISOString());
+
+    const { data: insertedEvent } = await this.client.events.insert({
+      calendarId: superKoyomiCalendarId,
+      requestBody: {
+        summary: event.summary,
+        start: { dateTime: event.start.toISOString() },
+        end: { dateTime: event.end.toISOString() },
+        attendees: event.attendees,
+      },
+    });
+
+    return {
+      id: insertedEvent.id ?? null,
+      summary: insertedEvent.summary ?? DEFAULT_EVENT.summary,
+      description: insertedEvent.description ?? null,
+      location: insertedEvent.location ?? null,
+      start: insertedEvent.start
+        ? new Date(insertedEvent.start.dateTime!)
+        : DEFAULT_EVENT.start,
+      end: insertedEvent.end
+        ? new Date(insertedEvent.end.dateTime!)
+        : DEFAULT_EVENT.end,
+      status: insertedEvent.status
+        ? this.validateEventStatus(insertedEvent.status)
+          ? insertedEvent.status
+          : null
+        : null,
+      attendees:
+        insertedEvent.attendees?.map((it) => ({ email: it.email ?? null })) ??
+        [],
+    };
   }
 
   private validateEventStatus(status: string): status is EventStatus {
