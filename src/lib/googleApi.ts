74ba6f3be1456063bce3dc2df0e1d35617148e99
uxiun <teppeiito0905@gmail.com>
2024-11-25 14:03:17 +0900

    changed OAuth scope

--- src/lib/googleApi.ts
+++ src/lib/googleApi.ts
@@ -7,8 +7,10 @@ if (!clientSecret) throw new Error("Missing GOOGLE_CLIENT_SECRET env var");
 export const SCOPE = [
   "https://www.googleapis.com/auth/userinfo.profile",
   "https://www.googleapis.com/auth/userinfo.email",
-  "https://www.googleapis.com/auth/calendar.readonly",
+  // "https://www.googleapis.com/auth/calendar.readonly",
   "https://www.googleapis.com/auth/calendar.app.created",
+  "https://www.googleapis.com/auth/calendar.freebusy",
+  "https://www.googleapis.com/auth/calendar.calendarlist.readonly",
 ];
 
 export { clientId as googleClientId, clientSecret as googleClientSecret };
