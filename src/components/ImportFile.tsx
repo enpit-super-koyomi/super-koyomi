666a5c10ef136982d4654b07d6f50d8cab205a35
uxiun <teppeiito0905@gmail.com>
2024-12-11 11:28:26 +0900

    search current courses

--- src/components/ImportFile.tsx
+++ src/components/ImportFile.tsx
@@ -1,68 +1,70 @@
 "use client"
 
-import { parseRSReferToCodes } from "@/lib/twinte-parser"
 import { useEffect, useState } from "react"
 import parseKDB, { Course } from "twinte-parser"
 
-export default function ImportFile({allCourses}: {allCourses: Course[]}) {
-  const [name, setName] = useState<string>()
-  const [contents, setContents] = useState<string>()
-  const [courses, setCourses] = useState<Course[]>([])
+const parseRSReferToCodes = (content: string): string[] =>
+	content.split("\n").map(line => line.replaceAll(/["\s\r]/gi, ""))
 
-  useEffect(() => {
-    if (!contents) return
-    const codes = parseRSReferToCodes(contents)
-    console.log("codes", codes)
-    // if (codes) setCourses(allCourses.filter(c => codes.includes(c.code)))
-  }, [contents])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files
-    if (!files || files.length === 0) return
-    const file = files[0]
-    setName(file.name)
+export default function ImportFile({ allCourses }: { allCourses: Course[] }) {
+	const [name, setName] = useState<string>()
+	const [contents, setContents] = useState<string>()
+	const [courses, setCourses] = useState<Course[]>([])
 
-    const reader = new FileReader()
-    reader.addEventListener("load", () => {
-      setContents(typeof reader.result == "string" ? reader.result : undefined)
-      console.log("contents:", contents)
+	const searchCourses = () => {
+		if (!contents) return
+		const codes = parseRSReferToCodes(contents)
+		console.log("codes", codes)
+		setCourses(allCourses.filter(c => codes.includes(c.code)))
+	}
 
-    })
+	useEffect(() => {
+		searchCourses()
+	}, [contents])
 
-    reader.readAsText(file)
-  }
-  return(
-  <div >
-    <input
-      type="file"
-      accept=".csv"
-      onChange={handleChange}
-    />
-    <div>
-      <p>content of {name}</p>
-      <pre
-        className="bg-slate-50"
-      >{contents}</pre>
-    </div>
-    <div>
-      <h3>your courses</h3>
-      {name == undefined ? <div>file has not uploaded</div> :
-      <Courses courses={courses} />}
-    </div>
-    <div>
-      <h3>all courses (head 10)</h3>
-      <Courses courses={allCourses.slice(0,10)} />
-    </div>
-  </div>)
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files
+		if (!files || files.length === 0) return
+		const file = files[0]
+		setName(file.name)
+
+		const reader = new FileReader()
+		reader.addEventListener("load", () => {
+			setContents(typeof reader.result == "string" ? reader.result : undefined)
+			console.log("contents:", contents)
+		})
+
+		reader.readAsText(file)
+	}
+	return (
+		<div>
+			<input type="file" accept=".csv" onChange={handleChange} />
+			<div>
+				<p>content of {name}</p>
+				<pre className="bg-slate-50">{contents}</pre>
+			</div>
+			<button onClick={searchCourses}>reload </button>
+			<div>
+				<h3>your courses</h3>
+				{name == undefined ? <div>file has not uploaded</div> : <Courses courses={courses} />}
+			</div>
+			<div>
+				<h3>all courses (head 10)</h3>
+				<Courses courses={allCourses.slice(0, 10)} />
+			</div>
+		</div>
+	)
 }
 
-const Courses = ({courses}: {courses: Course[]}) => {
-  return (
-    <div>{
-      courses.map(course =>
-        <div id={course.code}>
-          <pre>{JSON.stringify(course)}</pre>
-        </div>)
-    }</div>
-  )
+const Courses = ({ courses }: { courses: Course[] }) => {
+	return (
+		<div>
+			{courses.map(course => (
+				<div id={course.code}>
+					<pre>{JSON.stringify(course)}</pre>
+				</div>
+			))}
+		</div>
+	)
 }
--- src/lib/twinte-parser.ts
+++ src/lib/twinte-parser.ts
@@ -1,14 +1,13 @@
 "use server"
 
 import { downloadKDB, parseKDB } from "twinte-parser"
-const kdb = downloadKDB()
 
-export const loadCourses = async() => await parseKDB(await kdb);
+export const loadCourses = async(year?: number) => {
+  const kdb = await downloadKDB(year)
+  return await parseKDB(kdb)
+}
 
 export const getCourse = async(code: string) => {
   return (await loadCourses()).find(course => course.code == code)
 }
 
-export const parseRSReferToCodes = (content: string): string[] =>
-  content.split("\n")
-    .map(line => line.slice(1, -1));
