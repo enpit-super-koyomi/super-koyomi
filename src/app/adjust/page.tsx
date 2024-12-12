cbda3e2440b4056c45b541e76e3c28f745b11779
uxiun <teppeiito0905@gmail.com>
2024-12-13 02:00:22 +0900

    clean UI

--- src/app/adjust/page.tsx
+++ src/app/adjust/page.tsx
@@ -6,6 +6,7 @@ import { authOptions } from '@/lib/auth'
 export default async function AdjustPage() {
   const session = await getServerSession(authOptions)
   const users = (await db.allUsers())
-    .filter(user => user.email !== session?.user.email)
-  return <SchedulePlanner users={users} />
+    .filter(user => user.email !== session?.user.email);
+
+  return <SchedulePlanner users={users} session={session ?? undefined} />
 }
